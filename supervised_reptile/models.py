2322fd03b8301b5e5ff17ecd87fe714aa99c4e7b
Wojciech Glogowski <glogowski.wojciech@gmail.com>
2018-10-27 15:06:17 +0200

    progressive net under development

--- supervised_reptile/models.py
+++ supervised_reptile/models.py
@@ -49,3 +49,77 @@ class MiniImageNetModel:
                                                                    logits=self.logits)
         self.predictions = tf.argmax(self.logits, axis=-1)
         self.minimize_op = optimizer(**optim_kwargs).minimize(self.loss)
+
+
+# pylint: disable=R0903
+class ProgressiveOmniglotModel:
+    """
+    Progressive n-column model for Omniglot. Check https://arxiv.org/abs/1606.04671 for more details.
+    """
+
+    def __init__(self, num_classes, num_columns, optimizer=DEFAULT_OPTIMIZER, **optim_kwargs):
+        self.num_columns = num_columns
+        self.num_conv_layers = 4
+        self.input_ph = tf.placeholder(tf.float32, shape=(None, 28, 28))
+        self.results = {}
+        out = tf.reshape(self.input_ph, (-1, 28, 28, 1))
+        for col_idx in range(self.num_columns):
+            self.results[col_idx, 0] = out
+        for col_idx in range(self.num_columns):
+            for layer_idx in range(1, self.num_conv_layers + 2):
+                self.results[col_idx, layer_idx] = self.create_module(col_idx, layer_idx)
+
+    def create_module(self, col_idx, layer_idx):
+        if layer_idx == 1:
+            out = self.results[col_idx, layer_idx]
+            out = tf.layers.conv2d(out, 64, 3, strides=2, padding='same')
+            out = tf.layers.batch_normalization(out, training=True)
+            out = tf.nn.relu(out)
+            return out
+        elif layer_idx < self.num_conv_layers + 1:
+            laterals = [self.results[i, layer_idx - 1] for i in range(col_idx)]
+
+        else:
+            laterals = [self.results[i, layer_idx - 1] for i in range(col_idx)]
+
+
+    def logits(self, num_column):
+        return self.results[num_column, self.num_conv_layers]
+
+
+
+
+
+
+        for _ in range(num_layers):
+            out = tf.layers.conv2d(out, 64, 3, strides=2, padding='same')
+            out = tf.layers.batch_normalization(out, training=True)
+            out = tf.nn.relu(out)
+        out = tf.reshape(out, (-1, int(np.prod(out.get_shape()[1:]))))
+        self.logits = tf.layers.dense(out, num_classes)
+        self.label_ph = tf.placeholder(tf.int32, shape=(None,))
+        self.loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.label_ph,
+                                                                   logits=self.logits)
+        self.predictions = tf.argmax(self.logits, axis=-1)
+        self.minimize_op = optimizer(**optim_kwargs).minimize(self.loss)
+
+# pylint: disable=R0903
+class ProgressiveMiniImageNetModel:
+    """
+    Progressive n-column model for Mini-ImageNet. Check https://arxiv.org/abs/1606.04671 for more details.
+    """
+    def __init__(self, num_classes, num_columns, optimizer=DEFAULT_OPTIMIZER, **optim_kwargs):
+        self.input_ph = tf.placeholder(tf.float32, shape=(None, 84, 84, 3))
+        out = self.input_ph
+        for _ in range(4):
+            out = tf.layers.conv2d(out, 32, 3, padding='same')
+            out = tf.layers.batch_normalization(out, training=True)
+            out = tf.layers.max_pooling2d(out, 2, 2, padding='same')
+            out = tf.nn.relu(out)
+        out = tf.reshape(out, (-1, int(np.prod(out.get_shape()[1:]))))
+        self.logits = tf.layers.dense(out, num_classes)
+        self.label_ph = tf.placeholder(tf.int32, shape=(None,))
+        self.loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.label_ph,
+                                                                   logits=self.logits)
+        self.predictions = tf.argmax(self.logits, axis=-1)
+        self.minimize_op = optimizer(**optim_kwargs).minimize(self.loss)
